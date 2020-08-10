d4977f255e287dc2a6a1f7cfac4b78f969b7d7df
Habin <jerry19910626@163.com>
2020-08-10 22:14:03 +0800

    Milady

---
+++ Milady.py
@@ -0,0 +1,36 @@
+class actor:
+    name = "Milady"
+    sex = "female"
+    height = 169
+    locate = "Mage"
+    race = "human"
+
+    # skill = "jixiepucong"
+    # skill1 = "kongzhongzhiyuan"
+    # skill2 = "qiangzhiruqin"
+    # skill3 = "haojiecichang"
+
+
+    def walk(self):
+        # walk = "还有5秒到达战场"
+        print("还有5秒到达战场")
+        # return
+    def arrive(self):
+        # arrive = "到达战场"
+        print("到达战场")
+
+    def hit(self):
+        # hit = "补刀"
+        print("补兵")
+
+    def fight(self):
+        # fight = "对战敌方英雄"
+        print("对战敌方英雄")
+
+
+Milady = actor()
+print(actor.name)
+print(actor.race)
+print(actor.locate)
+# print(actor.walk("还有5秒到达战场"))
+
