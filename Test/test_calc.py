9a48d1608ca738eca05c769c179e1cad7333f1b9
Habin <jerry19910626@163.com>
2020-08-17 21:53:08 +0800

    calculator

--- Test/test_calc.py
+++ Test/test_calc.py
@@ -2,7 +2,6 @@ import pytest
 import yaml
 
 from pythoncode.calculator import calculator
-
 '''
 pytest命名规则：
 文件名：test开头（test_*.py)
@@ -10,14 +9,13 @@ pytest命名规则：
 方法名：test_开头
 '''
 
-
-# 读取测试数据
+#读取测试数据
 def get_datas():
-    with open('./datas/calculator.yml', encoding='utf-8') as f:
+    with open('./datas/calculator.yml',encoding='utf-8') as f:
         mydatas = yaml.safe_load(f)
         adddatas = mydatas['add']['datas']
         myids = mydatas['add']['myids']
-    return [adddatas, myids]
+    return [adddatas,myids]
 
 
 class Testcalc:
@@ -30,31 +28,32 @@ class Testcalc:
         self.calc = calculator()
 
     # @pytest.mark.add
-    @pytest.mark.parametrize('a,b,expect', get_datas()[0], ids=get_datas()[1])
-    def test_add(self, a, b, expect):
+    @pytest.mark.parametrize('a,b,expect',get_datas()[0],ids=get_datas()[1])
+    def test_add(self,a,b,expect):
         '''
         测试相加
         '''
         print("测试相加")
         # calc = calculator()
-        result = self.calc.add(a, b)
+        result = self.calc.add(a,b)
         assert expect == result
 
 
-# 读取测试数据
-def get_datas1():
-    with open('./datas/calculator.yml', encoding='utf-8') as f:
-        mydatas = yaml.safe_load(f)
-        divdatas = mydatas['div']['datas1']
-        myids1 = mydatas['div']['myids1']
-    return [divdatas, myids1]
 
-    @pytest.mark.parametrize('a,b,expect', get_datas1()[0], ids=get_datas1()[1])
-    def test_div(self, a, b, expect):
+    @pytest.mark.parametrize('a,b,expect',
+                             [
+                                 (0,1,0),
+                                 (0.1,2,0.05),
+                                 (101,10,10.1),
+                                 (0,-1,0),
+                                 # (0,1,1),
+                                 # (0,1,1),
+                             ],ids=['整数',  '小数', '大数', '零'])
+    def test_div(self,a,b,expect):
         '''
         测试相除
         '''
         print("测试相除")
         # calc = calculator()
-        result = self.calc.div(a, b)
+        result = self.calc.div(a,b)
         assert expect == result
